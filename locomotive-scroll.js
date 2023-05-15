0b8a9393651079b45ea2ad178c6eda2a32d3dab5
imanmachukwu <imanma@imanma.dev>
2023-05-15 09:39:57 +0100

    Activate locomotive scroll

--- locomotive-scroll.js
+++ locomotive-scroll.js
@@ -1,3 +1,8 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
-const scroll = new LocomotiveScroll();
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+console.log("yup");
